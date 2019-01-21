cd31327abb6c6c3419603c0209ae26a11572df98
Leonid Vlasov <leoniv.vlasov@gmail.com>
2019-01-21 15:53:52 +0700

    реализовал модуль Стек

--- ones/test/test_helper.rb
+++ ones/test/test_helper.rb
@@ -2,6 +2,24 @@ require 'hackathon'
 require 'ass_tests/minitest'
 require 'ass_ole/rubify'
 
+MiniTest::Spec.class_eval do
+  def self.shared_examples
+    @shared_examples ||= {}
+  end
+end
+
+module MiniTest::Spec::SharedExamples
+  def shared_examples_for(desc, &block)
+    MiniTest::Spec.shared_examples[desc] = block
+  end
+
+  def it_behaves_like(desc)
+    self.instance_eval(&MiniTest::Spec.shared_examples[desc])
+  end
+end
+
+Object.class_eval { include(MiniTest::Spec::SharedExamples) }
+
 AssTests::InfoBases.describe do
   external :hackathon, ENV['HACKATHON_UNDER_TEST'] do
     platform_require ENV['HACKATHON_PLATFORM_REQUIRE']
